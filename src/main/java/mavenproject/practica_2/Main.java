a77ee17f6b1520492293787e5beead66fcbc52b2
AlvaroMartinezQ <alvaroo2302@gmail.com>
2022-03-03 19:20:41 +0100

    Added comments

--- src/main/java/mavenproject/practica_2/Main.java
+++ src/main/java/mavenproject/practica_2/Main.java
@@ -2,7 +2,9 @@ package mavenproject.practica_2;
 
 public class Main {
     public static void main(String[] argv) {
+        // 5 son las dimensiones del vector
         Vectores test_v = new Vectores(5);
+        // Imprimimos para comprobar que todo esta en orden (que se ha ordenado aleatoriamente bien)
         test_v.imprime_vectores();
         Algor a = new Algor();
         System.out.println(
