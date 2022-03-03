d8018e3efab5c6c2a69e95fd6527f1b6b01e0143
AlvaroMartinezQ <alvaroo2302@gmail.com>
2022-03-03 19:19:28 +0100

    Inversion algorithm

--- src/main/java/mavenproject/practica_2/Main.java
+++ src/main/java/mavenproject/practica_2/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] argv) {
         Vectores test_v = new Vectores(5);
         test_v.imprime_vectores();
+        Algor a = new Algor();
+        System.out.println(
+            a.exec_fuerza(test_v.get_vector_2())
+        );
     }
 }
