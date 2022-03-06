921a97312522fccd1a5706297741ed6d0ac9e54b
Sebas1705 <100527906+Sebas1705@users.noreply.github.com>
2022-03-06 22:49:46 +0100

    Add files via upload

---
+++ src/main/java/mavenproject/practica_2/src/main/java/trabajofds2/Datos.java
@@ -0,0 +1,39 @@
+package trabajofds2;
+
+public class Datos {
+    
+    //Atributos:
+        private int inversiones;
+        private long tiempo;
+    //End Atributos.
+    
+    //Constructor:
+        public Datos(int inversiones, long tiempo){
+            this.inversiones = inversiones;
+            this.tiempo = tiempo;
+        }
+    //End Constructor.
+    
+    //Metodos públicos:
+        @Override
+        public String toString(){
+            StringBuilder sb = new StringBuilder();
+            sb.append("Inversiones: ").append(this.inversiones).append("\n");
+            sb.append("Tiempo(ns):  ").append(this.tiempo).append("\n");
+            return sb.toString();
+        }
+    //End Metodos públicos.
+    
+    //Metodos privados:
+    //End Metodos privados.
+    
+    //Getters:
+        public int getInversiones(){return this.inversiones;}
+        public long getTiempo(){return this.tiempo;}
+    //End Getters.
+    
+    //Setters:
+        public void setInversiones(int inversiones){this.inversiones = inversiones;}
+        public void setTiempo(long tiempo){this.tiempo = tiempo;}
+    //End Setters.
+}
