088d4f56a8df1feaa715d1f06c26ec27afde8301
Sebas1705 <100527906+Sebas1705@users.noreply.github.com>
2022-03-10 17:07:09 +0100

    Update Interfaz.java

--- src/pratica_2/Interfaz.java
+++ src/pratica_2/Interfaz.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package trabajofds2;
+package practica_2;
 
 
 import java.io.BufferedWriter;
