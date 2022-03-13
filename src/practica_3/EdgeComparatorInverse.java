b92866ca5e86070fd883a9ac9265146246c958d7
Sebas1705 <100527906+Sebas1705@users.noreply.github.com>
2022-03-13 11:48:04 +0100

    Update EdgeComparatorInverse.java

--- src/practica_3/EdgeComparatorInverse.java
+++ src/practica_3/EdgeComparatorInverse.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package practica_3;
 
-/**
- *
- * @author sebss
- */
 import java.util.Comparator;
 
 public class EdgeComparatorInverse implements Comparator<Edge> {
