d5cab3458e8dd12ffb1219dca8d808962bf49830
Sebas1705 <100527906+Sebas1705@users.noreply.github.com>
2022-03-13 11:47:37 +0100

    Update Main.java

--- src/practica_3/Main.java
+++ src/practica_3/Main.java
@@ -3,13 +3,13 @@ package practica_3;
 public class Main {
     public static void main(String[] args) {
         TicketToRide ticketToRide = new TicketToRide();
-        // ticketToRide.loadAllMap(); // carga Europa 
+        // ticketToRide.LoadEuropeMap(); // carga Europa 
         ticketToRide.loadIberianMap(); // carga peninsula Iberica
 
         //Metodo que resuelve Kruskal camino minimo
         ticketToRide.CalcMinSpanningTree();
         
         // Metodo que resuelve Kruskal (?) camino maximo
-        // ticketToRide.CalcMaxSpanningTree();
+        ticketToRide.CalcMaxSpanningTree();
     }
 }
