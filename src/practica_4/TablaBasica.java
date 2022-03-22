153d2708f1e034181715731edbb01331bf513e61
Sebas1705 <100527906+Sebas1705@users.noreply.github.com>
2022-03-22 17:55:57 +0100

    Update TablaBasica.java

--- src/practica_4/TablaBasica.java
+++ src/practica_4/TablaBasica.java
@@ -2,50 +2,38 @@ package trabajofds4;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.util.Scanner;
 
 public class TablaBasica extends TablaDatos{
     
-    private static final int Large = 20;
-    private static final int[] m = {3,4,5,6,3,3,5,3,3,3,3,4,4,5,6,8,6,6,10,12};
-    private static final int[] n = {3,4,5,6,10,12,6,4,5,6,8,6,3,3,3,3,4,5,3,3};
+    public static final int Large = 20, ps1 = 0, ps2 = 0;
+    public static final int[] m = {3,4,5,6,3,3,5,3,3,3,3,4,4,5,6,8,6,6,10,12};
+    public static final int[] n = {3,4,5,6,10,12,6,4,5,6,8,6,3,3,3,3,4,5,3,3};
     
-    public TablaBasica(int pos1, int pos2, int x){
+    public TablaBasica(){
         this.titulos = new String[Large];
         this.abierto = new boolean[Large];
         this.cerrado = new boolean[Large];
         this.tiempoAbierto = new long[Large];
         this.tiempoCerrado = new long[Large];
-        execPruebas(pos1, pos2, x);
+        
+        execPruebas();
+        
         generaTxT("TablaBasica.txt");
     }
     
-    private void execPruebas(int pos1, int pos2, int x){
+    private void execPruebas(){
         Caballo c = new Caballo();
-        int ps1 = pos1;
-        int ps2 = pos2;
-        Scanner sc = new Scanner(System.in);
         for(int i = 0; i < Large; i++){
-            if(x == 2){
-                do{
-                    System.out.println("Introduce la pos1, entre 0 y " + m[i]);
-                    ps1 = sc.nextInt();
-                }while(ps1 < 0 || ps1 > m[i]);
-                do{
-                    System.out.println("Introduce la pos2, entre 0 y " + n[i]);
-                    ps2 = sc.nextInt();
-                }while(ps2 < 0 || ps2 > n[i]);
-            }
             titulos[i] = m[i]+"x"+n[i];
-            Datos temp = c.buscaCamino(pos1, pos2, m[i], n[i], 1);
+            Datos temp = c.buscaCamino(ps1, ps2, m[i], n[i], 1);
             abierto[i] = temp.funciono;
             tiempoAbierto[i] = temp.tiempo;
-            temp = c.buscaCamino(pos1, pos2, m[i], n[i], 2);
+            temp = c.buscaCamino(ps1, ps2, m[i], n[i], 2);
             cerrado[i] = temp.funciono;
             tiempoCerrado[i] = temp.tiempo;
         }
-        sc.close();
     }
+    
     
     private void generaTxT(String nombre){
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(nombre))){
