77925da7dfa35dbc4b5fc9dde3fc0b3c15c29aff
Sebas1705 <100527906+Sebas1705@users.noreply.github.com>
2022-03-23 13:42:09 +0100

    Update Main.java

--- src/practica_4/Main.java
+++ src/practica_4/Main.java
@@ -1,11 +1,36 @@
 package practica_4;
 
+import java.util.Scanner;
+
 public class Main {
 	
-	public static void main(String[] argv) {
-		Caballo c = new Caballo();
-		c.execUsuario();
-		// c.execPruebas();
+        public static void main(String[] argv) {
+            Caballo c = new Caballo();
+            Scanner in = new Scanner(System.in);
+            boolean salir = false;
+            int sel;
+            do{
+                System.out.println("------Menu Seleccion:-----");
+                System.out.println("+1.Pruebas Personalizadas");
+                System.out.println("+2.Pruebas Tablas");
+                System.out.println("+3.Pruebas Globales");
+                System.out.println("+4-?.Salir");
+                System.out.println("--------------------------");
+                sel = in.nextInt();  
+                switch(sel){
+                    case 0: default:
+                        salir = true;
+                        break;
+                    case 1:
+                        c.execUsuario();
+                        break;
+                    case 2:
+                        c.execPruebas();
+                        break;
+                    case 3:
+                        c.execPruebasGlobales();
+                        break;
+                }
+            }while(!salir);
 	}
-
 }
