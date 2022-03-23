ceabd24fceb3efd062e4580e694e3742aed25f10
Sebas1705 <100527906+Sebas1705@users.noreply.github.com>
2022-03-23 13:41:37 +0100

    Update Datos.java

--- src/practica_4/Datos.java
+++ src/practica_4/Datos.java
@@ -3,9 +3,11 @@ package practica_4;
 public class Datos {
     public boolean funciono;
     public long tiempo;
+    public String tablero;
     
-    public Datos(boolean funciono, long tiempo){
+    public Datos(boolean funciono, long tiempo, String tablero){
         this.funciono = funciono;
         this.tiempo = tiempo;
+        this.tablero = tablero;
     }
 }
