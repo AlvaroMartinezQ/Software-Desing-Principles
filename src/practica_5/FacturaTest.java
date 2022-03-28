798978dfc91984f525ff9833019b263f09f3aa9f
Sebas1705 <100527906+Sebas1705@users.noreply.github.com>
2022-03-28 14:01:38 +0200

    Create FacturaTest.java

---
+++ src/practica_5/FacturaTest.java
@@ -0,0 +1,127 @@
+package practica_5;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import trabajofds5.*;
+
+public class FacturaTest {
+    
+    //Declaramos los cuatro objetos segun el iva y la deduccion:
+    FacturaFactorizada facNormalNormal;
+    FacturaFactorizada facNormalBonificada;
+    FacturaFactorizada facReducidoNormal;
+    FacturaFactorizada facReducidoBonificada;
+    
+    public FacturaTest() {
+        //Inicializamos cada objeto con su respectivos ivas y deducciones:
+        facNormalNormal = new FacturaFactorizada(new IvaNormal(), new DeduccionNormal());
+        facNormalBonificada = new FacturaFactorizada(new IvaNormal(), new DeduccionBonificada());
+        facReducidoNormal = new FacturaFactorizada(new IvaReducido(), new DeduccionNormal());
+        facReducidoBonificada = new FacturaFactorizada(new IvaReducido(), new DeduccionBonificada());
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here:
+    @Test
+    public void TestNN1(){
+        facNormalNormal.setImporteFactura(100);
+        facNormalNormal.setPorcentajeDeduccion(10);
+        assertEquals(106, facNormalNormal.CalcularTotal());
+    }
+    
+    @Test
+    public void TestNN2(){
+        facNormalNormal.setImporteFactura(200);
+        facNormalNormal.setPorcentajeDeduccion(20);
+        assertEquals(192, facNormalNormal.CalcularTotal());
+    }
+    
+    @Test
+    public void TestNN3(){
+        facNormalNormal.setImporteFactura(300);
+        facNormalNormal.setPorcentajeDeduccion(30);
+        assertEquals(258, facNormalNormal.CalcularTotal());
+    }
+    
+    @Test
+    public void TestNB1(){
+        facNormalBonificada.setImporteFactura(100);
+        facNormalBonificada.setPorcentajeDeduccion(10);
+        assertEquals(96, facNormalBonificada.CalcularTotal());
+    }
+    
+    @Test
+    public void TestNB2(){
+        facNormalBonificada.setImporteFactura(200);
+        facNormalBonificada.setPorcentajeDeduccion(20);
+        assertEquals(152, facNormalBonificada.CalcularTotal());
+    }
+    
+    @Test
+    public void TestNB3(){
+        facNormalBonificada.setImporteFactura(300);
+        facNormalBonificada.setPorcentajeDeduccion(30);
+        assertEquals(168, facNormalBonificada.CalcularTotal());
+    }
+    
+    @Test
+    public void TestRN1(){
+        facReducidoNormal.setImporteFactura(100);
+        facReducidoNormal.setPorcentajeDeduccion(10);
+        assertEquals(98, facReducidoNormal.CalcularTotal());
+    }
+    
+    @Test
+    public void TestRN2(){
+        facReducidoNormal.setImporteFactura(200);
+        facReducidoNormal.setPorcentajeDeduccion(20);
+        assertEquals(176, facReducidoNormal.CalcularTotal());
+    }
+    
+    @Test
+    public void TestRN3(){
+        facReducidoNormal.setImporteFactura(300);
+        facReducidoNormal.setPorcentajeDeduccion(30);
+        assertEquals(234, facReducidoNormal.CalcularTotal());
+    }
+    
+    @Test
+    public void TestRB1(){
+        facReducidoBonificada.setImporteFactura(100);
+        facReducidoBonificada.setPorcentajeDeduccion(10);
+        assertEquals(88, facReducidoBonificada.CalcularTotal());
+    }
+    
+    @Test 
+    public void TestRB2(){
+        facReducidoBonificada.setImporteFactura(200);
+        facReducidoBonificada.setPorcentajeDeduccion(20);
+        assertEquals(136, facReducidoBonificada.CalcularTotal());
+    }
+    
+    @Test 
+    public void TestRB3(){
+        facReducidoBonificada.setImporteFactura(300);
+        facReducidoBonificada.setPorcentajeDeduccion(30);
+        assertEquals(144, facReducidoBonificada.CalcularTotal());
+    }
+}
